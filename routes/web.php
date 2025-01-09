c6762f5e0e7460e99570f8087700447bcb890794
ammar sihombing <ammarmare01@gmail.com>
2025-01-09 19:57:44 +0700

    debugging

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// error no semicolon
+// Route::get('/hello', function () {
+//     return 'Hello World'
+// });
+
+
+// Attribute [gets] does not exist
+// Route::gets('/hello', function () {
+//     return 'Hello World';
+//  });
+
+
+
+// console.log(var_dump)
+// Route::get('/hello', function () {
+//     $hello = 'Hello World';
+//     var_dump($hello);
+//     return $hello;
+//     });
+
+
+// dd
+// Route::get('/hello', function () {
+//     $hello = 'Hello World';
+//     dd($hello);
+// });
+
+
+Route::get('hello', function () {
+    $hello = ['Hello World', 2 => ['Hello Jakarta','Hello Medan']];
+    dd($hello);
+    return $hello;
+    });
