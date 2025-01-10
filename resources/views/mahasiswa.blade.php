5c8b45c76640995f4359457e587db9fa83abf65f
ammar sihombing <ammarmare01@gmail.com>
2025-01-10 21:45:46 +0700

    if else php

--- resources/views/mahasiswa.blade.php
+++ resources/views/mahasiswa.blade.php
@@ -8,13 +8,19 @@
     <title>Data Mahasiswa</title>
 </head>
 <body>
-    <div class="container text-center mt-3 py-3 bg-white">
-        <h1 class="bg-dark px-3 py-1 text-white d-inline-block">{{$nama}}</h1>
-        <h1 class="bg-dark px-3 py-1 text-white d-inline-block">
-            {!! $nama !!}
-        </h1>
-        <h1 class="bg-dark px-3 py-1 text-white d-inline-block">{{$nilai}}</h1>
-        <h1 class="bg-dark px-3 py-1 text-white d-inline-block">{{ now()->format('Y-m-d H:i:s') }}</h1>
-    </div>
+    <div class="container text-center mt-3 pt-3 bg-white">
+        <h1 class="bg-dark px-3 py-1 text-white d-inline-block">{{ $nama }}</h1>
+        <h1 class="bg-dark px-3 py-1 text-white d-inline-block">{{ $nilai }}</h1>
+        <br>
+        <div class="alert alert-secondary d-inline-block">
+        <?php
+        if (($nilai >= 0) and ($nilai < 50)) {
+            echo "Maaf, anda tidak lulus";
+        }
+        else if (($nilai >= 50) and ($nilai <= 100)) {
+            echo "Selamat, anda lulus";
+        }
+        ?>
+<!-- End Generation Here -->
 </body>
 </html>
