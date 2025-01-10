5e937ab5a2d58670b16a6e34a35a125a77e61c35
ammar sihombing <ammarmare01@gmail.com>
2025-01-10 21:22:51 +0700

    blade without php tag

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/mahasiswa', function () {
+    $nama = 'Tya Kirana Putri';
+    $nilai = 75;
+    return view('mahasiswa',compact('nama','nilai'));
+    });
