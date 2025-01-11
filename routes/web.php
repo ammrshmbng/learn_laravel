4b26ad46ad75f9ad7b4bb8534eeb56e58249254e
ammar sihombing <ammarmare01@gmail.com>
2025-01-11 20:03:22 +0700

    without layout

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/mahasiswa', function () {
-    $nama = '<u>Tya Kirana Putri</u>';
-    $nilai = 75;
-    return view('mahasiswa',compact('nama','nilai'));
+Route::get('mahasiswa', function () {
+    $arrMahasiswa = ["Risa Lestari","Rudi Hermawan","Bambang Kusumo",
+    "Lisa Permata"];
+    return view('mahasiswa')->with('mahasiswa', $arrMahasiswa);
+    });
+    Route::get('dosen', function () {
+    $arrDosen = ["Maya Fitrianti, M.M.","Prof. Silvia Nst, M.Farm.",
+    "Dr. Umar Agustinus","Dr. Syahrial, M.Kom."];
+    return view('dosen')->with('dosen', $arrDosen);
+    });
+    Route::get('gallery', function () {
+    return view('gallery');
     });
