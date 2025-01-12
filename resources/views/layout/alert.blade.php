f2e5e1d440b60734f6cc1e0f5e87d6ea0e49e7de
ammar sihombing <ammarmare01@gmail.com>
2025-01-12 22:44:09 +0700

    laravel extention pack

--- resources/views/layout/alert.blade.php
+++ resources/views/layout/alert.blade.php
@@ -1,4 +1,4 @@
-<div class="alert alert-{{$class}} alert-dismissible fade show">
-  {{$slot}}
-  <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+<div class="alert alert-{{ $class }} alert-dismissible fade show">
+    {{ $slot }}
+    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -18,20 +18,28 @@ Route::get('/', function () {
 });
 
 Route::get('mahasiswa', function () {
-    $arrMahasiswa = ["Risa Lestari","Rudi Hermawan","Bambang Kusumo",
-    "Lisa Permata"];
+    $arrMahasiswa = [
+        "Risa Lestari",
+        "Rudi Hermawan",
+        "Bambang Kusumo",
+        "Lisa Permata"
+    ];
     return view('mahasiswa')->with('mahasiswa', $arrMahasiswa);
-    });
-    Route::get('dosen', function () {
-    $arrDosen = ["Maya Fitrianti, M.M.","Prof. Silvia Nst, M.Farm.",
-    "Dr. Umar Agustinus","Dr. Syahrial, M.Kom."];
+});
+Route::get('dosen', function () {
+    $arrDosen = [
+        "Maya Fitrianti, M.M.",
+        "Prof. Silvia Nst, M.Farm.",
+        "Dr. Umar Agustinus",
+        "Dr. Syahrial, M.Kom."
+    ];
     return view('dosen')->with('dosen', $arrDosen);
-    });
-    Route::get('gallery', function () {
+});
+Route::get('gallery', function () {
     return view('gallery');
-    });
+});
 
 
-    Route::get('admin', function () {
-        return view('admin');
-    });
+Route::get('admin', function () {
+    return view('admin');
+});
