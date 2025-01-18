1389e41ab520c1b31d8d690980df568647595a1f
ammar sihombing <ammarmare01@gmail.com>
2025-01-18 12:40:14 +0700

    crud operations

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/mahasiswas', [MahasiswaController::class,'index'])
+->name('mahasiswas.index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/mahasiswas/create', [MahasiswaController::class,'create'])
+->name('mahasiswas.create');
+
+Route::post('/mahasiswas', [MahasiswaController::class,'store'])
+->name('mahasiswas.store');
+
+Route::get('/mahasiswas/{mahasiswa}', [MahasiswaController::class,'show'])
+->name('mahasiswas.show');
+
+Route::get('/mahasiswas/{mahasiswa}/edit', [MahasiswaController::class,'edit'])
+->name('mahasiswas.edit');
+
+Route::put('/mahasiswas/{mahasiswa}', [MahasiswaController::class,'update'])
+->name('mahasiswas.update');
+
+Route::delete('/mahasiswa/{mahasiswa}', [MahasiswaController::class,'destroy'])
+->name('mahasiswas.destroy');
