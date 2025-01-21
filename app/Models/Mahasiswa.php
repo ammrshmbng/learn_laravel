6ebd6a750502d692073ac6fcdbc5609ae359f98d
ammar sihombing <ammarmare01@gmail.com>
2025-01-21 14:29:06 +0700

    add debugbar

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
 
 class Mahasiswa extends Model
 {
     use HasFactory;
+    public function nilai(): HasOne
+    {
+        return $this->hasOne('App\Models\Nilai');
+    }
 }
